77fedd441008af799f1e02a23f1a2875a883a4ba
Georgios <test@example.com>
2021-05-03 18:09:40 +0200

    fix arrays

--- index.php
+++ index.php
@@ -3,34 +3,59 @@
 /**
  * HTTP headers
  */
-header("Content-Type: application/json; charset=utf-8");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: GET");
-header("Access-Control-Allow-Origin: Origin, X-Requested-With, Content-type, Accept");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
 
 /**
  * Create arrayer
  */
-$cardname = ["Howling mine", "Megrim"];
-$color = ["None", "Black"];
-$released_at = ["2015-01-23", "2000-05-15"];
-$lang = ["en", "en"];
+$cardName = ["Feasting Troll King", "Embercleave", "Kenrith, the Returned King", "Brazen Borrower", "Ayara, First of Locthwain", "Questing Beast", "Linden, the Steadfast Queen", "Stolen by the Fae", "Witch's Vengeance", "Torbran, Thane of Red Fell"];
+$color = ["Green", "Red", "White", "Blue", "Black", "Green", "White", "Blue", "Black", "Red"];
+$released_at = ["2021-01-23", "2020-05-15", "2019-01-23", "2018-05-15", "2017-01-23", "2017-01-23", "2021-01-23", "2020-05-15", "2019-01-23"];
+$lang = ["en", "en", "en", "en", "en", "en", "en", "en", "en", "en"];
 $image = [
-    "https://mtgrandom.com/ups/uploads/ths-158-Feral-Invocation.png",
-    "https://mtgrandom.com/ups/uploads/mb1-1079-Taurean-Mauler.png"
+    "https://media.wizards.com/2019/eld/en_vsxkO1cHgt.png",
+    "https://media.wizards.com/2019/eld/en_KrPDEzg9I9.png",
+    "https://media.wizards.com/2019/eld/en_9sk4qNBCgb.png",
+    "https://media.wizards.com/2019/eld/en_Pen4Mwa1ay.png",
+    "https://media.wizards.com/2019/eld/en_fwaFYG5GGO.png",
+    "https://media.wizards.com/2019/eld/en_JIl0E01Ulh.png",
+    "https://media.wizards.com/2019/eld/en_ugXoH2A186.png",
+    "https://media.wizards.com/2019/eld/en_YaSulArnbm.png",
+    "https://media.wizards.com/2019/eld/en_b5MarCvEEl.png",
+    "https://media.wizards.com/2019/eld/en_z4OExIIYVo.png"
 ];
 
 $cards = array();
 
 for ($i = 0; $i < 10; $i++) {
+    // $cardName = $cardName;
+    // $color = $color;
+    // $released_at = $released_at;
+    // $lang = $lang;
+    // $image = $image;
+
     $card = array(
-        "cardname" => $cardname,
-        "color" => $color,
-        "released_at" => $released_at,
-        "lang" => $lang,
-        "image" => $imga
+        "cardname" => $cardName[$i],
+        "color" => $color[$i],
+        "released_at" => $released_at[$i],
+        "lang" => $lang[$i],
+        "image" => $image[$i]
     );
     array_push($cards, $card);
 }
+
+/**
+ * Convert array to json
+ */
+$json = json_encode($cards, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+
+/**
+ * Send json to client
+ */
+echo $json;
